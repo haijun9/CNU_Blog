41ac1cec42a724adb2ccf732f9f42c36fcc16dc6
haijun9 <haijun79@naver.com>
2023-06-05 19:55:39 +0900

    숙제 try

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,11 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h4>실전코딩 수강</h4>
+      <span>2023.03 ~ 2023.06</span>
+      <span>React, SpringBoot 등 다양한 실무 관련 지식 수강</span>
+    </div>
+  );
 };
 
 export default Resume;
